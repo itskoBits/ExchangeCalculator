126716febe20bbe8bbd5571693b5d1b31dce31bb
Ivelin Davidov <ivelin.davidov@paysafe.com>
2019-01-23 13:21:10 +0200

    Delete code implementation.

--- ExchangeCalculator/ViewController.swift
+++ ExchangeCalculator/ViewController.swift
@@ -10,33 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var usdTextField: UITextField!
-    @IBOutlet var btcTextField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        usdTextField.addTarget(self, action: #selector(usdTextFieldDidChange), for: .editingChanged)
     }
     
-    @objc func usdTextFieldDidChange() {
-        let usdText = usdTextField.getText()
-        if let usdAmount = Decimal(string: usdText) {
-            let calculator = ExchangeCalculator()
-            let btcAmount = calculator.getBtcAmount(usdAmount: usdAmount)
-            
-            let btcAmountFormatter = NumberFormatter()
-            btcAmountFormatter.maximumFractionDigits = 8
-            btcAmountFormatter.minimumIntegerDigits = 1
-            
-            let btcText = btcAmountFormatter.string(from: btcAmount as NSNumber)
-            btcTextField.text = btcText
-        }
-    }
-    
-    @IBAction func didTapExchangeButton(_ sender: Any) {
-        let alert = UIAlertController(title: "Congrats!", message: "You successfully bought \(btcTextField.getText())", preferredStyle: .alert)
-        
-        present(alert, animated: true, completion: nil)
-    }
 }
